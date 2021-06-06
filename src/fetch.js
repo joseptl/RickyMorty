1fbf43d59324a994cdf18ff07865bd46c36a4f85
Jose Torin <joseptl551@gmail.com>
2021-06-06 02:47:30 -0400

    first commit

---
+++ src/fetch.js
@@ -0,0 +1,44 @@
+const url = "https://rickandmortyapi.com/api/character";
+
+export async function getCharactersByName (filtro){
+    const data = await fetch(`${url}/${filtro}`)
+    const lista = await data.json()
+    return lista.results
+}
+
+export async function printCharacter(filtro){
+    const listaPersonajes = document.querySelector(".characterList")
+    const $fragmento = document.createDocumentFragment();
+    const personajes = getCharactersByName(filtro)
+    const lista = await personajes
+    lista.forEach(element => {
+        const $contenedor=document.createElement("div");
+        $contenedor.classList.add("container-character");
+        const $contenedorData=document.createElement("div");
+        $contenedorData.classList.add("container-character-data");
+        const $data=document.createElement("div");
+        const $contenedorImagen=document.createElement("div");
+        $contenedorImagen.classList.add("container-character_Containerimage")
+        const $imagen=document.createElement("img");
+        $imagen.src=`${element.image}`;
+        $imagen.alt=`Foto de Perfil de ${element.name}`
+        $data.classList.add("container-character_info");
+        $data.innerHTML=
+        `
+        <ul class="dataList">
+            <li class="dataListItem">Name: ${element.name}</li>
+            <li class="dataListItem">Status: ${element.status}</li>
+            <li class="dataListItem">Gender: ${element.gender}</li>
+            <li class="dataListItem">Species: ${element.species}</li>
+        </ul>`
+        $contenedorImagen.appendChild($imagen);
+        $contenedorData.appendChild($data);
+        $contenedorData.appendChild($contenedorImagen);
+        $contenedor.appendChild($contenedorData)
+        $fragmento.appendChild($contenedor);
+    });
+    listaPersonajes.innerHTML="";
+    listaPersonajes.appendChild($fragmento);
+}
+
+export default printCharacter;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import printCharacter from "./fetch.js";
+const $formButtom=document.querySelector(".formButton");
+const filtro=()=>{
+    let busqueda=`?`
+    let cont=0
+    const lista = [...document.querySelectorAll(".formInput")]
+    lista.forEach(element => {
+        if(!element.value==""&&cont===0){
+            busqueda=`${busqueda}${element.name}=${element.value.toLowerCase()}`
+            cont++
+        }
+        else if(!element.value==""){
+            busqueda=`${busqueda}&${element.name}=${element.value}`
+        }
+    });
+    return busqueda
+}
+$formButtom.addEventListener("click",function(){
+    printCharacter(filtro())
+})
