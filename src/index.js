f7fdaba820e7f0616f9c32288be2dd20629fcc0a
Jose Torin <joseptl551@gmail.com>
2021-06-26 11:37:23 -0400

    segundaprueba

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import printCharacter from "./fetch.js";
 const $formButtom=document.querySelector(".formButton");
 const filtro=()=>{
-    console.log("prueba")
     let busqueda=`?`
     let cont=0
     const lista = [...document.querySelectorAll(".formInput")]
